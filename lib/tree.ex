0ad11949afd4fcb8616a9cd2f6c1b2e49f274589
XoronEdge <ta.xit@yandex.com>
2019-05-09 15:33:16 +0500

    implement add node to tree and refactor code

---
+++ lib/tree.ex
@@ -0,0 +1,17 @@
+alias TreeNode
+
+root = %TreeNode{data: 1}
+
+root = TreeNode.add_node(root, 2)
+root = TreeNode.add_node(root, 3)
+root = TreeNode.add_node(root, 4)
+root = TreeNode.add_node(root, 5)
+root = TreeNode.add_node(root, 6)
+root = TreeNode.add_node(root, 7)
+root = TreeNode.add_node(root, 8)
+root = TreeNode.add_node(root, 9)
+root = TreeNode.add_node(root, 10)
+root = TreeNode.add_node(root, 11)
+root = TreeNode.add_node(root, 12)
+root = TreeNode.add_node(root, 13)
+IO.inspect(root.rightNode.leftNode.rightNode.data)
