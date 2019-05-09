19113a7e10d9494d917d8fe8134813c91a7c9a1c
XoronEdge <ta.xit@yandex.com>
2019-05-09 16:29:45 +0500

    refactor code and add module for better structurre

--- lib/tree.ex
+++ lib/tree.ex
@@ -1,18 +1,10 @@
-alias TreeNode
+defmodule Tree do
+  alias AddNode
+  alias TreeNode
 
-root = %TreeNode{data: 1}
+  def create_tree do
+    %TreeNode{data: 1}
+  end
 
-root = TreeNode.add_node(root, 2)
-root = TreeNode.add_node(root, 3)
-root = TreeNode.add_node(root, 4)
-root = TreeNode.add_node(root, 5)
-root = TreeNode.add_node(root, 6)
-root = TreeNode.add_node(root, 7)
-root = TreeNode.add_node(root, 8)
-root = TreeNode.add_node(root, 9)
-root = TreeNode.add_node(root, 10)
-root = TreeNode.add_node(root, 11)
-root = TreeNode.add_node(root, 12)
-root = TreeNode.add_node(root, 13)
-root = TreeNode.add_node(root, 14)
-IO.inspect(root.rightNode.leftNode.rightNode.data)
+  def add_new_node(%TreeNode{} = user, data), do: AddNode.add(user, data)
+end
